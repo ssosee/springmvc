a3b53c6facffbe637cbc0b9ec3ff0b80b88fec4f
Jang seaung <toughsingo@gmail.com>
2022-07-20 17:11:24 +0900

    스프링 MVC - 기본 기능  - HTTP 요청 파라미터 완료

--- README.md
+++ README.md
@@ -38,20 +38,20 @@ public class className {
 * @PutMapping
 * @DeleteMapping
 * @PatchMapping
-<br>
 
-* GET - 쿼리 파라미터
-    * /url?username=hello&age=20
-    * 메시지 바디 없이, URL의 쿼리 파라미터에 데이터를 포함해서 전달
-    * e.g) 검색, 필터, 페이징 등에서 많이 사용하는 방식
-* POST
-    * content-type: application/x-www-form-urlencoded
-    * 메시지 바디에 쿼리 파라미터 형식으로 전달 username=hello&age=20
-    * e.g) 회원가입, 상품 주문, HTML Form 사용
-* HTTP message body에 데이터를 직접 담아서 요청
-    * HTTP API 에서 주로 사용, JSON, XML, TEXT
-    * 데이터 형식은 주로 JSON 사용
-    * POST, PUT, PATCH
+
+* **GET - 쿼리 파라미터**
+  * /url?username=hello&age=20
+  * 메시지 바디 없이, URL의 쿼리 파라미터에 데이터를 포함해서 전달
+  * e.g) 검색, 필터, 페이징 등에서 많이 사용하는 방식
+* **POST**
+  * content-type: application/x-www-form-urlencoded
+  * 메시지 바디에 쿼리 파라미터 형식으로 전달 username=hello&age=20
+  * e.g) 회원가입, 상품 주문, HTML Form 사용
+* **HTTP message body에 데이터를 직접 담아서 요청**
+  * HTTP API 에서 주로 사용, JSON, XML, TEXT
+  * 데이터 형식은 주로 JSON 사용
+  * POST, PUT, PATCH
 
 *경로 변수(다중) 사용*
 ```java
---
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,127 @@
+package hello.springmvc.basic.request;
+
+import hello.springmvc.basic.HelloData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        log.info("username={}", username);
+        log.info("age={}", age);
+
+        response.getWriter().write("ok");
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(@RequestParam("username") String username,
+                               @RequestParam("age") int age) {
+
+        log.info("username={}", username);
+        log.info("age={}", age);
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(@RequestParam String username,
+                                 @RequestParam int age) {
+
+        log.info("username={}", username);
+        log.info("age={}", age);
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+
+        log.info("username={}", username);
+        log.info("age={}", age);
+
+        return "ok";
+    }
+
+    /**
+     * @RequestParam.required
+     * /request-param-required -> username이 없으므로 예외
+     *
+     * 주의!
+     * /request-param-required?username= -> 빈문자로 통과
+     *
+     * 주의!
+     * /request-param-required
+     * int age -> null을 int에 입력하는 것은 불가능, 따라서 Integer 변경해야 함(또는 다음에 나오는
+    defaultValue 사용)
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) Integer age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamRequired(@RequestParam(defaultValue = "guest") String username,
+                                       @RequestParam(defaultValue = "-1") int age) {
+
+        log.info("username={}", username);
+        log.info("age={}", age);
+
+        return "ok";
+    }
+
+    /**
+     * @RequestParam Map, MultiValueMap
+     * Map(key=value)
+     * MultiValueMap(key=[value1, value2, ...]) ex) (key=userIds, value=[id1, id2])
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+
+        log.info("username={}", paramMap.get("username"));
+        log.info("age={}", paramMap.get("age"));
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username = {}", helloData.getName());
+        log.info("age = {}", helloData.getAge());
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) {
+        log.info("username = {}", helloData.getName());
+        log.info("age = {}", helloData.getAge());
+
+        return "ok";
+    }
+}
